28fd585fac81f302d8990f494bb8a89de6e673d4
Matt Gamble <matt_gamble07@hotmail.com>
2011-11-03 10:16:21 -0500

    Updated gemfiles and readme

---
+++ sample_app1/config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Neworkspace::Application.config.session_store :cookie_store, key: '_neworkspace_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Neworkspace::Application.config.session_store :active_record_store
