487d7a675af0a2cebc4844b7130652922619007a
Matt Gamble <matt_gamble07@hotmail.com>
2011-11-03 09:52:56 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Neworkspace::Application.config.secret_token = '0efd4ab8d8805e61b2d4936e30fc221c9182b5193c4e34d7ba9eff32ccc891b885a0c63fd9489c3ce5c8bcf8df947dc261ae8103390af42b2fd59c4bb674dfa7'
